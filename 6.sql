08dc9643f0cbe95ebb56d69e668f0bb51dca957c
venkateshch543 <112920789+venkateshch543@users.noreply.github.com>
2022-09-07 19:49:49 +0530

    Add files via upload
    qui2to6answers

---
+++ 6.sql
@@ -0,0 +1,19 @@
+﻿Winner
+------------------------------
+Reinhard Selten
+Donald J.cram
+Jean-Marie Lehn
+Joseph Brodsky
+Kenzaburo Oe
+Robert solow
+Gerhard Herzberg
+Pabola Neruda
+Simon Kuznets
+aleksander solzhneitsyn
+Luis Fedrico Leoir
+Paul Samuelson
+
+(12 rows affected)
+
+
+Completion time: 2022-09-07T19:47:02.9965113+05:30
